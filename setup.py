8837cd4bc2d6a12a04a2ea1726796b899db6da8b
Polash <polashtechai@gmail.com>
2025-08-02 20:02:05 +0600

    project launch

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Polash',
+    author_email= 'polashtechai@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
